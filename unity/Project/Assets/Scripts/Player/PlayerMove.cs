ca34bfb77c27eaa5355c88cbfbc87e4196799b35
Alex <alex@plato>
2015-01-24 23:22:05 +0000

    Imported animation pack, duck with left shift, grid for basic floor

---
+++ unity/Project/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace Ggj.Player {
+	public class PlayerMove : MonoBehaviour {
+
+		static class StateTags {
+			public const string Idle = "Idle";
+            public const string Ducking = "Ducking";
+		}
+
+		static class AnimatorParams {
+	        public const string ShouldHop = "ShouldHop";
+            public const string ShouldIdlePant = "ShouldIdlePant";
+            public const string IsDucking = "IsDucking";
+		}
+
+        Animator animator;
+
+		void Start () {
+			animator = GetComponent<Animator> ();
+		}
+
+        bool IsIdle {
+            get {
+                return animator.GetCurrentAnimatorStateInfo (0).IsTag (StateTags.Idle);
+            }
+        }
+
+        bool IsDucking {
+            get {
+                return animator.GetCurrentAnimatorStateInfo (0).IsTag (StateTags.Ducking);
+            }
+        }
+
+		void Update () {
+            float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+
+            bool noMoveControl =
+				Math.Abs (moveVertical) < 0.01f &&
+				Math.Abs (moveHorizontal) < 0.01f;
+
+            bool duckControl = Input.GetAxis ("Duck") > 0.01f;
+
+            animator.SetBool (AnimatorParams.IsDucking, duckControl);
+
+            if (IsIdle && !noMoveControl) {
+                transform.rotation = CalculateNewRotation ();
+                animator.SetTrigger (AnimatorParams.ShouldHop);
+            } else if (IsIdle && duckControl) {
+                animator.SetBool (AnimatorParams.IsDucking, true);
+            } else if (IsDucking && !duckControl) {
+                animator.SetBool (AnimatorParams.IsDucking, false);
+            }
+
+			
+
+		}
+
+		Quaternion CalculateNewRotation() {
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+
+			double rawMoveDirectionRadians = Math.Atan2 (moveVertical, moveHorizontal);
+			double rawMoveDirection = (360 / (2 * Math.PI)) * rawMoveDirectionRadians;
+			double moveDirectionQuantized = 90 * Math.Floor (rawMoveDirection / 90);
+			double moveDirection = 90 - moveDirectionQuantized;
+
+			var rotation = transform.rotation;
+			var angles = rotation.eulerAngles;
+			angles.y = (float) moveDirection;
+			rotation.eulerAngles = angles;
+			return rotation;
+		}
+	}
+}
---
+++ unity/Project/Assets/Scripts/Player/RagdollScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//A very simple script to show how to turn a Mecanim character into a ragdoll
+//when the user presses space, assuming that the Unity Ragdoll Wizard has
+//been used to add the ragdoll RigidBody components
+
+namespace Ggj.Player {
+	public class RagdollScript : MonoBehaviour {
+
+		private float timerEnd = 5f;
+		private float timer = 0f;
+		public bool ragdoll = false;
+		
+		//Helper to set the isKinematic property of all RigidBodies in the children of the 
+		//game object that this script is attached to
+		void SetKinematic(bool newValue)
+		{
+			//Get an array of components that are of type Rigidbody
+			Rigidbody[] bodies=GetComponentsInChildren<Rigidbody>();
+			
+			//For each of the components in the array, treat the component as a Rigidbody and set its isKinematic property
+			foreach (Rigidbody rb in bodies)
+			{
+				rb.isKinematic=newValue;
+			}
+		}
+
+		void Start () {
+			//Set all RigidBodies to kinematic so that they can be controlled with Mecanim
+			//and there will be no glitches when transitioning to a ragdoll
+			SetKinematic(true);
+		}
+		
+		void Update () 
+		{
+			timer += Time.deltaTime;
+			if( timer > timerEnd)
+			{
+				ragdoll = true;
+			}
+			if ( ragdoll )
+			{
+				SetKinematic(false);
+				GetComponent<Animator>().enabled=false;
+			}
+		}
+	}
+}
