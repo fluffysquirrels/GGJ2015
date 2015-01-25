211e0a6c991c226aa2eef2801c2cbd4bb3db6c5b
Jonathan Raymond Westlake <jongaming@me.com>
2015-01-25 11:29:48 +0000

    added safe square

---
+++ unity/Project/Assets/Prefabs/SafeSquare/KillTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Ggj.Player;
+
+namespace Ggj.Prefabs {
+	public class KillTrigger : MonoBehaviour {
+	
+		void OnTriggerEnter(Collider c) {
+			var playerBehaviour = c.GetComponent<PlayerMove> ();
+			
+			if (playerBehaviour == null) {
+				// Only interested in colliding with player.
+				return;
+			}
+			
+			playerBehaviour.Kill ();
+		}
+	}
+}
