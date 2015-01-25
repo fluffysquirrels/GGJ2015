1e938a72bc2bc9561a57c20741d01340273e865d
Jonathan Raymond Westlake <jongaming@me.com>
2015-01-25 11:45:46 +0000

    Safe squares up and running

---
+++ unity/Project/Assets/Prefabs/SafeSquare/MainTrigger.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using Ggj.Player;
+
+namespace Ggj.Prefabs {
+	public class MainTrigger : MonoBehaviour {
+	
+		private Animator anim;
+		private float TimerEnd = 10f;
+		private float TimerCurrent = 0f;
+		private bool StartTimer = false;
+		
+		// Use this for initialization
+		void Start () 
+		{
+			anim = GetComponentInChildren<Animator>();
+		}
+		
+		// Update is called once per frame
+		void Update () 
+		{
+			if ( StartTimer )
+			{
+				TimerCurrent += Time.deltaTime;
+			}
+		
+			if ( TimerCurrent >= TimerEnd )
+			{
+				anim.SetTrigger("ShouldAttack");
+				TimerCurrent = 0f;
+				StartTimer = false;
+			}
+		}
+		
+		void OnTriggerEnter(Collider c) {
+			var playerBehaviour = c.GetComponent<PlayerMove> ();
+			
+			if (playerBehaviour == null) {
+				// Only interested in colliding with player.
+				return;
+			}
+			LetsStartTimer();
+		}
+		
+		void LetsStartTimer()
+		{
+			StartTimer = true;
+		}
+	}
+}
