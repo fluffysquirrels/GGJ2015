b83d0698ef8a2bb2118f5cdccbf2b156c9d806d0
Jonathan Raymond Westlake <jongaming@me.com>
2015-01-25 14:16:53 +0000

    MainMenu

---
+++ unity/Project/Assets/Scripts/MainMenuHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuHandler : MonoBehaviour {
+
+	public void LoadLevel(string level)
+	{
+		Application.LoadLevel(level);
+	}
+	
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
