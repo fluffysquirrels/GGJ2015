9c6d2045d529dae0d086c7ae9a72b026bd0f8321
Jonathan Raymond Westlake <jongaming@me.com>
2015-01-25 13:18:37 +0000

    boom

---
+++ unity/Project/Assets/Scripts/UIHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour {
+
+	public Text textComp;
+	private float TimerEnd = 8f;
+	private float Timer = 0f;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		textComp = GetComponent<Text>();
+		textComp.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	private void StartTimer() {
+	
+	}
+}
