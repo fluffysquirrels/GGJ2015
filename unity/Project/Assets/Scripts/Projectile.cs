7b6760c5f8cb6d7a249c4a7bb404c7d0d85c29dc
Alex <alex@plato>
2015-01-26 12:24:43 +0000

    Cubes stop bullets.

---
+++ unity/Project/Assets/Scripts/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Ggj.Scripts {
+    public class Projectile : MonoBehaviour {
+
+        void OnTriggerEnter(Collider other) {
+            if (other.GetComponent<StopsProjectiles> () != null) {
+                Object.Destroy (this.gameObject);
+            }
+        }
+    }
+}
---
+++ unity/Project/Assets/Scripts/StopsProjectiles.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using Ggj.Player;
+using Ggj.Prefabs;
+
+namespace Ggj.Scripts {
+    /// <summary>
+    /// This is just a marker class understood by projectiles.
+    /// </summary>
+    public class StopsProjectiles : MonoBehaviour {
+    }
+}
