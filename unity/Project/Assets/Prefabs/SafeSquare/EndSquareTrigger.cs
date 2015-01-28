3e107c15f8d276c7fac1ea8c5570d405d635a98d
Alex <alex@plato>
2015-01-28 17:19:52 +0000

    Created end square.

---
+++ unity/Project/Assets/Prefabs/SafeSquare/EndSquareTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Ggj.Player;
+
+namespace Ggj.Prefabs {
+    public class EndSquareTrigger : MonoBehaviour {
+        void OnTriggerEnter(Collider c) {
+            var playerBehaviour = c.GetComponent<PlayerMove> ();
+
+            if (playerBehaviour == null) {
+                // Only interested in colliding with player.
+                return;
+            }
+
+            Application.LoadLevel ("MainMenu");
+        }
+	}
+}
