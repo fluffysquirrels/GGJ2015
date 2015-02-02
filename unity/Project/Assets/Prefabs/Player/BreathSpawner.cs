c620b7109d19df56bc4dd3f29421c4759b2e6707
Jonathan Raymond Westlake <jongaming@me.com>
2015-02-02 21:23:44 +0000

    Added breath spawner script
    !! Had to tweak safe square wait time slightly to test breath particle
    system

---
+++ unity/Project/Assets/Prefabs/Player/BreathSpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreathSpawner : MonoBehaviour 
+{
+
+	// holds our breath particle system prefab reference
+	public GameObject breathEmitter;
+	
+	private GameObject _emitter;
+	private Animator _anim;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		_anim = transform.root.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( IsPanting() )
+		{	
+			if ( !_emitter )
+			{
+				Debug.Log ("Spawn");
+				// spawn emitter
+				_emitter = (GameObject)Instantiate( breathEmitter, transform.position, transform.rotation );
+				_emitter.transform.parent = transform;
+			}
+		} else
+		{
+			Debug.Log ("Destroy");
+			// destroy emitter
+			Destroy( _emitter );
+			_emitter = null;
+		}
+	}
+	
+	public bool IsPanting()
+	{
+		return _anim.GetCurrentAnimatorStateInfo(0).IsName( "IdlePant_Cycle" ) || _anim.GetCurrentAnimatorStateInfo(0).IsName( "IdlePant_Into" );
+	}
+}
