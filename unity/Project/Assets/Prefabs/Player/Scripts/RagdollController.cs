482e3872235cd37cd9e5aa994a669ada8fdd4462
Jonathan Raymond Westlake <jongaming@me.com>
2015-02-23 20:46:25 +0000

    Fixed broken respawn input on rag doll
    Respawn input was only on player object - I’ve added it to the  player
    ragdoll object as well

---
+++ unity/Project/Assets/Prefabs/Player/Scripts/RagdollController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RagdollController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Process commands
+		if (Input.GetKeyDown(KeyCode.Escape)) 
+		{
+			Application.LoadLevel("MainMenu");
+		}
+		if (Input.GetAxis ("Restart") > 0.01f) 
+		{
+			RestartLevel ();
+		}
+	}
+
+	void RestartLevel () {
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
+
